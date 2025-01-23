91ba896f999a07d6fef4676d6a3d26f13ac48cdf
Craig McKeachie <craigdmckeachie@gmail.com>
2025-01-22 19:03:15 -0500

    lab09

--- src/projects/ProjectCard.jsx
+++ src/projects/ProjectCard.jsx
@@ -7,6 +7,11 @@ function formatDescription(description) {
 
 function ProjectCard(props) {
   const { project } = props;
+
+  const handleEditClick = (projectBeingEdited) => {
+    console.log(projectBeingEdited);
+  };
+
   return (
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
@@ -16,6 +21,15 @@ function ProjectCard(props) {
         </h5>
         <p>{formatDescription(project.description)}</p>
         <p>Budget : {project.budget.toLocaleString()}</p>
+        <button
+          className="bordered"
+          onClick={() => {
+            handleEditClick(project);
+          }}
+        >
+          <span className="icon-edit "></span>
+          Edit
+        </button>
       </section>
     </div>
   );
