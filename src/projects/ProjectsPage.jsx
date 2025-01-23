31b9ba84127254bd09606157355fdf4bf6523b31
Craig McKeachie <craigdmckeachie@gmail.com>
2025-01-23 11:09:00 -0500

    lab19

--- src/projects/ProjectsPage.jsx
+++ src/projects/ProjectsPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import ProjectList from './ProjectList';
 import { projectAPI } from './projectAPI';
+import { Project } from './Project';
 
 function ProjectsPage() {
   const [projects, setProjects] = useState([]);
@@ -33,10 +34,17 @@ function ProjectsPage() {
   };
 
   const saveProject = (project) => {
-    let updatedProjects = projects.map((p) => {
-      return p.id === project.id ? project : p;
-    });
-    setProjects(updatedProjects);
+    projectAPI
+      .put(project)
+      .then((updatedProject) => {
+        let updatedProjects = projects.map((p) => {
+          return p.id === project.id ? new Project(updatedProject) : p;
+        });
+        setProjects(updatedProjects);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
   };
   return (
     <>
--- src/projects/projectAPI.js
+++ src/projects/projectAPI.js
@@ -1,16 +1,16 @@
-import { Project } from "./Project";
+import { Project } from './Project';
 
-const baseUrl = "http://localhost:4000";
+const baseUrl = 'http://localhost:4000';
 const url = `${baseUrl}/projects`;
 
 function translateStatusToErrorMessage(status) {
   switch (status) {
     case 401:
-      return "Please login again.";
+      return 'Please login again.';
     case 403:
-      return "You do not have permission to view the project(s).";
+      return 'You do not have permission to view the project(s).';
     default:
-      return "There was an error retrieving the project(s). Please try again.";
+      return 'There was an error retrieving the project(s). Please try again.';
   }
 }
 
@@ -43,19 +43,39 @@ function delay(ms) {
 
 const projectAPI = {
   get(page = 1, limit = 20) {
-    return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-    //   .then(delay(600))
+    return (
+      fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+        //   .then(delay(600))
+        .then(checkStatus)
+        .then(parseJSON)
+        .then((projects) => {
+          return projects.map((p) => {
+            return new Project(p);
+          });
+        })
+        .catch((error) => {
+          console.log('log client error ' + error);
+          throw new Error(
+            'There was an error retrieving the projects. Please try again.'
+          );
+        })
+    );
+  },
+
+  put(project) {
+    return fetch(`${url}/${project.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(project),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
       .then(checkStatus)
       .then(parseJSON)
-      .then((projects) => {
-        return projects.map((p) => {
-          return new Project(p);
-        });
-      })
       .catch((error) => {
-        console.log("log client error " + error);
+        console.log('log client error ' + error);
         throw new Error(
-          "There was an error retrieving the projects. Please try again."
+          'There was an error updating the project. Please try again.'
         );
       });
   },
